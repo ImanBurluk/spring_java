c0ab0b520691ac8e28187b707eda2a76b666b8be
ImanBurluk <jaroslavzev63@mail.ru>
2024-06-30 18:28:33 +0500

    Dependency Injection with setter by value

--- src/main/java/com/github/imanburluk/MusicPlayer.java
+++ src/main/java/com/github/imanburluk/MusicPlayer.java
@@ -3,6 +3,25 @@ package com.github.imanburluk;
 public class MusicPlayer {
     private Music music;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    private String name;
+    private int volume;
+
 //    public MusicPlayer(Music music) {
 //        this.music = music;
 //    }
