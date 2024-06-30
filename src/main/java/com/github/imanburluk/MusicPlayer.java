f81f888968835009380167f6346219fc7bd297f8
ImanBurluk <jaroslavzev63@mail.ru>
2024-06-30 18:21:44 +0500

    Dependency Injection with setter

--- src/main/java/com/github/imanburluk/MusicPlayer.java
+++ src/main/java/com/github/imanburluk/MusicPlayer.java
@@ -3,11 +3,19 @@ package com.github.imanburluk;
 public class MusicPlayer {
     private Music music;
 
-    public MusicPlayer(Music music) {
+//    public MusicPlayer(Music music) {
+//        this.music = music;
+//    }
+
+    public MusicPlayer() {
+
+    }
+
+    public void setMusic(Music music) {
         this.music = music;
     }
 
     public void playMusic() {
-        System.out.println("Playng"+ music.getSong());
+        System.out.println("Playng: "+ music.getSong());
     }
 }
